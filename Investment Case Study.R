3f4193cc6af4edb7b951ec888b5b67dbeabd38f9
BalajiDeepanandha <34003761+BalajiDeepanandha@users.noreply.github.com>
2017-11-27 23:57:36 +0100

    Update Investment Case Study.R

--- Investment Case Study.R
+++ Investment Case Study.R
@@ -1,5 +1,5 @@
-companies <- read.delim("companies.txt", header = TRUE)
-rounds2 <- read.csv("rounds2.csv", stringsAsFactors = FALSE)
+companies <- read.delim("companies.txt", header = TRUE, na.strings=c("","NA") )
+rounds2 <- read.csv("rounds2.csv", stringsAsFactors = FALSE, na.strings=c("","NA"))
 
 #Table 1.1: Understand the Data Set
 #1. How many unique companies are present in rounds2?
@@ -43,5 +43,5 @@ venture_country_groups <- group_by(venture, country_code)
 vcg_total_funding <- summarise(venture_country_groups, sum(raised_amount_usd, na.rm = T))
 
 names(vcg_total_funding) <- c("country_code","total_funding_amt")
-
-arrange(top_n(vcg_total_funding, 10, total_funding_amt), desc(total_funding_amt))
+vcg_total_funding <- na.omit(vcg_total_funding)
+arrange(top_n(vcg_total_funding, 9, total_funding_amt), desc(total_funding_amt))
